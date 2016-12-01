cda5d6537b63bca620ebd056ec09fa99499a631a
Matt Bateman <mbateman@laughingsky.com>
2016-12-01 15:50:36 +0000

    added device type

---
+++ type.js
@@ -0,0 +1,21 @@
+function from_sys(typesCollection,devicesCollection,deviceID,data) {
+	console.log("Type Change");
+	var typeID = data.typeID;
+	typesCollection.findOne({typeID: typeID})
+		.then((typeDoc) => {
+			console.log("Type updated with findOneAndUpdate");
+			devicesCollection.findOneAndUpdate({deviceID: deviceID},
+				{$set: {req_state: typeDoc.req_state, config: typeDoc.config}})
+				.then((doc) => {
+					console.log("Device type intialized with findOneAndUpdate");
+				}).catch((err) => {
+					console.log("Error with findOneAndUpdate <"+err+">");
+				});
+		}).catch((err) => {
+			console.log("Error with findOne <"+err+">");
+		});
+
+}
+
+module.exports.from_sys = from_sys;
+
